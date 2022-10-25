4505af671f94379efb8d01a7b042c5255f330fa9
Antoine Adde <Antoine.Adde@unil.ch>
2022-10-25 22:18:36 +0200

    update data doc

---
+++ R/data_covfilter-data.R
@@ -0,0 +1,15 @@
+#' Example covsel.filter output data
+#'
+#' A ready-to-use output from the covsel.filter function
+#'
+#' @docType data
+#'
+#' @usage data(data_covfilter)
+#'
+#' @format A data frame containing 37 filtered covariates at 13609 locations 
+#'
+#' @author Antoine Adde, 2022-10-16
+#'
+#' @keywords datasets
+#'
+"data_covfilter"
---
+++ R/data_covsel-data.R
@@ -0,0 +1,15 @@
+#' Generic data for running the main covsel example
+#'
+#' Ready-to-use input data for running covsel demonstration
+#'
+#' @docType data
+#'
+#' @usage data(data_covsel)
+#'
+#' @format A list containing a "pa" vector with 3609 species presences (1) and 10k background absences (0); and data frame with 75 candidate covariates at the 13609 locations 
+#'
+#' @author Antoine Adde, 2022-10-16
+#'
+#' @keywords datasets
+#'
+"data_covsel"
